63139b25061b41ff7b43884430a79cdfd31b7ceb
kaminijajal <kaminijajal14@gmail.com>
2026-06-26 11:06:16 +0530

    react & js 26-6

---
+++ Javascript/Assignment/Session 3/1.js
@@ -0,0 +1,6 @@
+let likes = 1000;
+let comments = 270;
+
+let totalengagement = likes + comments;
+
+console.log("Total Engagement : ",totalengagement);
---
+++ Reactjs/Assignment/Session 1/firstreactapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+        <h1>"Welcome to My React Zomato App"</h1>
+  );
+}
+
+export default App;
